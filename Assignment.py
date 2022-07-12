28d90db9ea65a642bab810b1041cec3d609fc221
KarinaOluwakay <temiangelu@gmail.com>
2022-07-12 10:42:26 +0100

    First Commit

---
+++ Assignment.py
@@ -0,0 +1,10 @@
+name = input("Angel Oluwakayode: ")
+print(name)
+
+# Output:
+# Enter your name: Angel
+# Angel
+
+name("Angel")
+print(len(name))
+print =("0+4")
---
+++ first class1.py
@@ -0,0 +1,11 @@
+print("Hello World")
+print("Hello World\n"+" My name is Angel\n" +" I am 13 years old\n "+ "I want to study science in he university" )
+print("Oh sing"*-5)
+
+name = "Christopher"
+print(len(name))
+
+
+
+RESEARCH ON INPUT FUNCTIONS, AND USE THE INPUT FUNCTION TO RECIEVE 2 USER INPUTS AS I NTERGERS AND STORE THE INPUT IN TWO VARIABLES,
+MULTIPLY THE TWO VARIABLES AND OUTPUT THE ANSWER
